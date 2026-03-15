bff3c413a94b2631ccd205f1dc812da6212e9a54
Emin-Yurtlu <155135740+Emin-Yurtlu@users.noreply.github.com>
2026-03-15 15:15:59 +0300

    kullanıcı oluşturma işlemi gercekleştirildi.

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using IdentityApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityApp.Controllers
@@ -12,10 +13,38 @@ namespace IdentityApp.Controllers
         {
             _userManager = userManager;
         }
+        [HttpGet]
         public IActionResult Index()
         {
             return View(_userManager.Users);
         }
 
-    } 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = model.UserName, Email = model.Email };
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (IdentityError err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+
+                }
+            }
+
+                return View(model);
+            
+        }
+    }
 }
